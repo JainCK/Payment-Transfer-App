d2ada135b381bce23ae0f07ec740bc649c9c0aa6
Jain K <kuriakosejain456@gmail.com>
2024-01-22 21:17:30 +0530

    Added accout schema

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,48 @@
 import mongoose from "mongoose";
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI)
 
 const userSchema = new mongoose.Schema({
-username: String,
-password: String,
-firstName: String,
-lastName: String
+username: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+},
+password: {
+    type: String,
+    required: true,
+    minLength: 6
+},
+firstName: {
+    type: String,
+    required: true,
+    trim: true,
+},
+lastName: {
+    type: true,
+    required: true,
+    trim: true,
+}
 
 })
 
-const User = mongoose.model('User', userSchema);
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type : userSchema.Types.objectId, 
+        ref: "User",
+        required :true
+    }, 
+    balance: {
+        type: Number,
+        required: true
+    }
+});
 
-module.exports = User;
+
+
+const User = mongoose.model('User', userSchema);
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = { User, Account };
