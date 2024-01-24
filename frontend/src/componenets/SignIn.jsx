eada57884372a28ff7253ca66f2e55b835923058
Jain K <kuriakosejain456@gmail.com>
2024-01-24 22:23:50 +0530

    style fix

--- frontend/src/componenets/SignIn.jsx
+++ frontend/src/componenets/SignIn.jsx
@@ -12,11 +12,11 @@ const SignIn = () => {
             <p className='text-center text-lg mt-3 text-gray-500'>Enter your credentials to access your account</p>
             <hr className='mt-3'/>
             <div className='mt-3'>
-                <label htmlFor="email" className='block text-base mb-2 font-bold'>Email</label>
+                <label htmlFor="email" className='block text-base mb-2 font-semibold'>Email</label>
                 <input type="text" id='email' className='border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600' placeholder='johndoe@example.com'/>
             </div>
             <div className='mt-3'>
-                <label htmlFor="password" className='block text-base mb-2 font-bold'>Password</label>
+                <label htmlFor="password" className='block text-base mb-2 font-semibold'>Password</label>
                 <input type="text" id='password' className='border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600' placeholder=''/>
             </div>
             <div className='mt-5'>
