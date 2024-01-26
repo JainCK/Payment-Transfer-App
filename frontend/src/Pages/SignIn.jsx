b2ad3a98fd04a90520bf0cd8d49d76442f0950d9
Jain K <kuriakosejain456@gmail.com>
2024-01-26 22:38:26 +0530

    Added Links

--- frontend/src/Pages/SignIn.jsx
+++ frontend/src/Pages/SignIn.jsx
@@ -1,37 +1,63 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import '../index.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "../index.css";
 
 const SignIn = () => {
   return (
     <>
-    <div className='flex justify-center items-center h-screen bg-gray-500'>
-    <div className='w-1/4 p-6 shadow-lg bg-white rounded-md'>
-            <h1 className='text-center font-extrabold text-3xl'>Sign In</h1>
-            <p className='text-center text-lg mt-3 text-gray-500'>Enter your credentials to access your account</p>
-            <hr className='mt-3'/>
-            <div className='mt-3'>
-                <label htmlFor="email" className='block text-base mb-2 font-semibold'>Email</label>
-                <input type="text" id='email' className='border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600' placeholder='johndoe@example.com'/>
-            </div>
-            <div className='mt-3'>
-                <label htmlFor="password" className='block text-base mb-2 font-semibold'>Password</label>
-                <input type="text" id='password' className='border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600' placeholder=''/>
-            </div>
-            <div className='mt-5'>
-                <button className='w-full bg-black text-white rounded-md h-10'>Sign In</button>
-            </div>
-            <div className='mt-3'>
-                <p className='text-center font-semibold'>
-                    Don't have an account? Sign Up
-                </p>
-            </div>
+      <div className="flex justify-center items-center h-screen bg-gray-500">
+        <div className="w-1/4 p-6 shadow-lg bg-white rounded-md">
+          <h1 className="text-center font-extrabold text-3xl">Sign In</h1>
+          <p className="text-center text-lg mt-3 text-gray-500">
+            Enter your credentials to access your account
+          </p>
+          <hr className="mt-3" />
+          <div className="mt-3">
+            <label
+              htmlFor="email"
+              className="block text-base mb-2 font-semibold"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600"
+              placeholder="johndoe@example.com"
+            />
+          </div>
+          <div className="mt-3">
+            <label
+              htmlFor="password"
+              className="block text-base mb-2 font-semibold"
+            >
+              Password
+            </label>
+            <input
+              type="text"
+              id="password"
+              className="border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600"
+              placeholder=""
+            />
+          </div>
+          <div className="mt-5">
+            <button className="w-full bg-black text-white rounded-md h-10">
+              Sign In
+            </button>
+          </div>
+          <div className="mt-3">
+            <p className="text-center font-semibold">
+              Don't have an account? {""}
+              <Link to="/" className="underline text-blue-500">
+                Sign Up
+              </Link>
+            </p>
+          </div>
         </div>
-    </div>
-       
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,59 +1,103 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+  });
 
-    const [formData, setFormData] = useState({
-        username: '',
-        firstName: '',
-        lastName:'',
-        password: '',
-    });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value })
-    }
-
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   return (
     <>
-    <div className='flex justify-center items-center h-screen bg-gray-500'>
-    <div className='w-1/4 p-6 shadow-lg bg-white rounded-md'>
-            <h1 className='text-center font-bold text-3xl'>Sign Up</h1>
-            <p className='text-center text-lg mt-3 text-gray-500'>Enter your information to create an account</p>
-            <hr className='mt-3'/>
-            
-            <div className='mt-3'>
-                <label htmlFor="firstName" className='block text-base mb-2 font-semibold'>First Name</label>
-                <input type="text" id='firstName' className='border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600' placeholder='John'/>
-            </div>
-            <div className='mt-3'>
-                <label htmlFor="lastName" className='block text-base mb-2 font-semibold'>Last Name</label>
-                <input type="text" id='lastName' className='border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600' placeholder='Doe'/>
-            </div>
-            <div className='mt-3'>
-                <label htmlFor="email" className='block text-base mb-2 font-semibold'>Email</label>
-                <input type="text" id='email' className='border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600' placeholder='johndoe@example.com'/>
-            </div>
-            <div className='mt-3'>
-                <label htmlFor="password" className='block text-base mb-2 font-semibold'>Password</label>
-                <input type="text" id='password' className='border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600' placeholder=''/>
-            </div>
-            <div className='mt-5'>
-                <button className='w-full bg-black text-white rounded-md h-10'>Sign Up</button>
-            </div>
-            <div className='mt-3'>
-                <p className='text-center font-semibold'>
-                    Already have an account? Login
-                </p>
-            </div>
-        </div>
-    </div>    
-    </>
-  )
-}
+      <div className="flex justify-center items-center h-screen bg-gray-500">
+        <div className="w-1/4 p-6 shadow-lg bg-white rounded-md">
+          <h1 className="text-center font-bold text-3xl">Sign Up</h1>
+          <p className="text-center text-lg mt-3 text-gray-500">
+            Enter your information to create an account
+          </p>
+          <hr className="mt-3" />
 
-export default SignUp
+          <div className="mt-3">
+            <label
+              htmlFor="firstName"
+              className="block text-base mb-2 font-semibold"
+            >
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              className="border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600"
+              placeholder="John"
+            />
+          </div>
+          <div className="mt-3">
+            <label
+              htmlFor="lastName"
+              className="block text-base mb-2 font-semibold"
+            >
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              className="border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600"
+              placeholder="Doe"
+            />
+          </div>
+          <div className="mt-3">
+            <label
+              htmlFor="email"
+              className="block text-base mb-2 font-semibold"
+            >
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600"
+              placeholder="johndoe@example.com"
+            />
+          </div>
+          <div className="mt-3">
+            <label
+              htmlFor="password"
+              className="block text-base mb-2 font-semibold"
+            >
+              Password
+            </label>
+            <input
+              type="text"
+              id="password"
+              className="border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600"
+              placeholder=""
+            />
+          </div>
+          <div className="mt-5">
+            <button className="w-full bg-black text-white rounded-md h-10">
+              Sign Up
+            </button>
+          </div>
+          <div className="mt-3">
+            <p className="text-center font-semibold">
+              Already have an account? {""}
+              <Link to="/signin" className="underline text-blue-500">
+                Log In
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
