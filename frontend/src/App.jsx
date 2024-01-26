2408ba88046b5caa4b9a99240a537d94339fb5a8
Jain K <kuriakosejain456@gmail.com>
2024-01-26 22:06:33 +0530

    imported transfer comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import Dashboard from './componenets/Dashboard'
 import SignIn from './componenets/SignIn'
 import SignUp from './componenets/SignUp'
+import Transfer from './componenets/Transfer'
 import './index.css'
 function App() {
 
   return (
     <>
     {/* <SignIn /> 
-    <SignUp /> */}
-    <Dashboard />
+    <SignUp /> 
+    <Dashboard /> */}
+    <Transfer />
     </>
   )
 }  
