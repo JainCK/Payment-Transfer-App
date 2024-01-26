2fc054a15dc249607e3d1ecffb955fe20081a5a0
Jain K <kuriakosejain456@gmail.com>
2024-01-26 22:38:01 +0530

    Added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import Dashboard from './Pages/Dashboard'
-import SignIn from './Pages/SignIn'
-import SignUp from './Pages/SignUp'
-import Transfer from './Pages/Transfer'
-import './index.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Transfer from "./Pages/Transfer";
+import "./index.css";
 function App() {
-
   return (
-    <>
-    {/* <SignIn /> 
-    <SignUp /> 
-    <Transfer />*/}
-  <Dashboard /> 
-    </>
-  )
-}  
+    <Routes>
+      <Route path="/" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
 
-export default App
+export default App;
