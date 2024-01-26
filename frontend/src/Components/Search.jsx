2e6d90be2c287b94482e42bb7cd9b21e8aa23505
Jain K <kuriakosejain456@gmail.com>
2024-01-26 22:38:17 +0530

    spell Fix

--- frontend/src/Components/Search.jsx
+++ frontend/src/Components/Search.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
     <>
-         <div className='my-5 border'>
-            <h2 className='font-bold text-2xl ml-10'>Users</h2>
-            <input type="text" class="h-8 w-4/5 mx-10 mt-5 border-slate-700 rounded text-lg" placeholder="Search Users..." />
-        </div>  
+      <div className="my-5 border">
+        <h2 className="font-bold text-2xl ml-10">Users</h2>
+        <input
+          type="text"
+          className="h-8 w-4/5 mx-10 mt-5 border-slate-700 rounded text-lg"
+          placeholder="Search Users..."
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
