4ab393d3f832c2b7776edd74886181b47b851991
Jain K <kuriakosejain456@gmail.com>
2024-01-28 23:08:06 +0530

    Balance comp done

--- frontend/src/Components/Balance.jsx
+++ frontend/src/Components/Balance.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Balance = () => {
+  const [balance, setBalance] = useState("");
+
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      axios
+        .get("http://localhost:3000/api/v1/account/balance", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then(
+          (response) => {
+            setBalance(response.data.balance);
+          },
+          (error) => {
+            console.error("Error fetching balance:", error);
+          }
+        );
+    };
+
+    fetchBalance();
+  }, [token]);
+
   return (
     <>
-          <div className='my-5 border '>
-            <h2 className='font-bold text-2xl ml-10'>Your Balance: </h2>
-        </div>  
+      <div className="my-5 border ">
+        <h2 className="font-bold text-2xl ml-10">
+          Your Balance: {balance !== null ? balance : "Loading..."}
+        </h2>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Balance
+export default Balance;
