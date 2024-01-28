51773d18eb1ad5d512439801da380d8f9701ef4c
Jain K <kuriakosejain456@gmail.com>
2024-01-28 21:56:23 +0530

    Inputbox comp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./Pages/Dashboard";
 import SignIn from "./Pages/SignIn";
 import SignUp from "./Pages/SignUp";
+import Dashboard from "./Pages/Dashboard";
+import Transfer from "./Pages/Transfer";
 import "./index.css";
+
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/transfer" element={<Transfer />} />
     </Routes>
   );
 }
---
+++ frontend/src/Components/Inputbox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Inputbox = ({ label, placeholder }) => {
+  return (
+    <div>
+      <div className="text-sm font-medium text-left py-2">{label}</div>
+      <input
+        className="border w-full text-base px-2 py-1 focus: outline-none focus:ring-0 focus:border-gray-600"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+};
+
+export default Inputbox;
